2e76683eb65b158bc9da4bd835b9ae4405adf18a
Elvis Nuñez <elvisnunez@me.com>
2015-03-18 13:48:36 +0100

    Add isEqual method

--- Source/HYPParsedRelationship.m
+++ Source/HYPParsedRelationship.m
@@ -2,5 +2,12 @@
 
 @implementation HYPParsedRelationship
 
+- (BOOL)isEqual:(HYPParsedRelationship *)object
+{
+    return ((!self.relationship || [self.relationship isEqualToString:object.relationship]) &&
+            self.index == object.index &&
+            self.toMany == object.toMany &&
+            (!self.attribute || [self.attribute isEqualToString:object.attribute]));
+}
 
 @end
